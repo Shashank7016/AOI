83755cc0cf00e1265600fc39cab97abbc5cbd40b
Shashank7016 <bryanhovak@example.com>
2020-06-16 14:00:52 +0530

    Updates

---
+++ precache-manifest.8456aa515c6bfadbd86ce7b38058948c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949e5099b6b072610514b0ffdbc64f0e",
+    "url": "/AOI/index.html"
+  },
+  {
+    "revision": "c77da99b3c373723cfe3",
+    "url": "/AOI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c5117ff70425da45bcec",
+    "url": "/AOI/static/js/2.5f46c166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AOI/static/js/2.5f46c166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77da99b3c373723cfe3",
+    "url": "/AOI/static/js/main.b2e6564c.chunk.js"
+  },
+  {
+    "revision": "a8f980fef01c96e50610",
+    "url": "/AOI/static/js/runtime-main.f073f191.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AOI/static/media/logo.5d5d9eef.svg"
+  }
+]);
